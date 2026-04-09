e716711c65526953320905aa188b0db0b5a30c97
jun-01111 <scrt1025@gmail.com>
2026-04-09 16:49:28 +0900

    feat: 용수 이용 효율 및 자원 순환율 분석 툴 구현

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,13 +1,23 @@
-# ESG 평가 도구 모음
-# 각 팀원이 개발한 툴을 이 파일에 import해서 tools 리스트에 추가.
-#
-# 예시:
-# from .search_guideline import search_guideline
-# from .calculate_carbon import calculate_carbon
-#
-# tools 리스트에 추가된 툴은 Agent가 자동으로 인식합니다.
-
+# 1. 각 파일에서 실제 정의된 함수 이름을 정확히 임포트합니다.
 from .calculate_demo import calculate_carbon_emission
 from .web_search import web_search_esg
+from .water_recycling import analyze_water_resource_circulation
+from .search_PDF import search_pdf_tool  # 아연님의 RAG 도구 추가
 
-tools = [calculate_carbon_emission, web_search_esg]
+# 2. 에이전트가 인식할 도구 리스트 정의
+# nodes.py에서 이 리스트를 가져가서 LLM에게 바인딩합니다.
+tools = [
+    calculate_carbon_emission,
+    web_search_esg,
+    analyze_water_resource_circulation,
+    search_pdf_tool
+]
+
+# 3. 패키지 외부 노출 설정
+__all__ = [
+    "tools", 
+    "calculate_carbon_emission", 
+    "web_search_esg", 
+    "analyze_water_resource_circulation",
+    "search_pdf_tool"
+]
